6eef41a559e3d87475fe7feee50f12cfc4d2f935
ART89-prog <dykpgilkom@mail.ru>
2022-05-05 22:24:32 +0300

    Доделал

--- js/scripts.js
+++ js/scripts.js
@@ -37,4 +37,20 @@ $(() => {
     });
 
 
+    $('header .btn').on('click', function() {
+        let href = $(this).attr('href');    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, {
+            duration: 870,  
+            easing: "linear" 
+        });
+    
+        return false;
+    });
+
+
+
+
+
 })
